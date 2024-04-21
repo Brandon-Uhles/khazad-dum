137459f29cf8ee27e6957a9155cb938145979229
Brandon-Uhles <--global>
2024-04-21 15:35:36 -0400

    feat(fov): add field of view funcitonality
    Introduce Viewshed component to manage player field of view. Set up
    FovSystem system to handle field of view calculations and previous view
    memory.

--- src/ecs/systems.rs
+++ src/ecs/systems.rs
@@ -1,21 +1,40 @@
-use crate::map::{xy_idx, TileType};
+use crate::map::{Map, TileType};
 
 // module for all systems
-use super::components::{LeftMover, Player, Position};
+use super::components::{Player, Position, Viewshed};
+use rltk::{field_of_view, Point };
 use specs::prelude::*;
 use std::cmp::{max, min};
 
-pub struct LeftWalker {}
+pub struct FoVSystem {}
 
-impl<'a> System<'a> for LeftWalker {
-    // requests read and write access for the specified components from the system data
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+impl<'a> System<'a> for FoVSystem {
+    type SystemData = 
+        (WriteExpect<'a, Map>,
+        Entities<'a>,
+        WriteStorage<'a, Viewshed>,
+        WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>);
+    fn run(&mut self, data : Self::SystemData) {
+        let (mut map,entities, mut viewshed, pos, player) = data;
+        
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join(){
+            if viewshed.dirty{
+                viewshed.dirty = false;
+                viewshed.visible_tiles.clear();
+                viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+                viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height );
 
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
+                    // If this is the player, reveal what they can see
+                let _p : Option<&Player> = player.get(ent);
+                if let Some(_p) = _p {
+                    for t in map.visible_tiles.iter_mut() { *t = false };
+                    for vis in viewshed.visible_tiles.iter() {
+                        let idx = map.xy_idx(vis.x, vis.y);
+                        map.revealed_tiles[idx] = true;
+                        map.visible_tiles[idx] = true;
+                    }
+                }
             }
         }
     }
@@ -24,13 +43,16 @@ impl<'a> System<'a> for LeftWalker {
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            viewshed.dirty = true;
         }
     }
 }
